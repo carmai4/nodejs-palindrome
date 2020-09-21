ccd97fbc5986da318bbfeaef7bec7c691192686a
Caroline Maiorana <caroline.maiorana@gmail.com>
2020-09-21 16:14:17 -0400

    handle invalid argument

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function print(str) {
 }
 
 const input = minimist(process.argv.slice(2))._[0];
+if (!input) {
+  process.exit(9);
+} else if (typeof(input) !== 'string') {
+  process.exit(1);
+}
 const output = `${input} -> ${util.backwards(input)}`;
 print(output);
 print(`is a palindrome? ${util.stringsAreSame(input, util.backwards(input))}`);
